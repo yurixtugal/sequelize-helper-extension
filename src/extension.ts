c57e03a7b3002327cbc466321ee2b002f653b2c7
yurixtugal <yurixtugal12@gmail.com>
2024-01-19 01:10:52 -0500

    Find models directory

--- src/extension.ts
+++ src/extension.ts
@@ -17,10 +17,52 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from SequelizeExtensionHelper!');
+
+	});
+
+	let generateIndexFile = vscode.commands.registerCommand('sequelizeextensionhelper.generateIndex', async () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		
+		const arrWorkspaceFolders = vscode.workspace.workspaceFolders;
+
+		if (arrWorkspaceFolders && arrWorkspaceFolders.length > 0) {
+			const workspaceFolder = arrWorkspaceFolders[0];
+			const workspaceFolderPath = `${workspaceFolder.uri.path}/src`;
+			console.log(workspaceFolderPath);
+			const modelPath = await findModelFolder(workspaceFolderPath);
+			const arrModels = await vscode.workspace.fs.readDirectory(vscode.Uri.parse(modelPath));
+			console.log(arrModels);
+		}
+
+		vscode.window.showInformationMessage('Hello World from SequelizeExtensionHelper!');
+
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(generateIndexFile);
 }
+
+const findModelFolder = async (workspaceFolderPath: string): Promise<string> => {
+	const entries = await vscode.workspace.fs.readDirectory(vscode.Uri.parse(workspaceFolderPath));
+	
+	for (const [name, type] of entries) {
+		console.log(name, type);
+		if (type === vscode.FileType.Directory && ['model','models'].includes(name.toLocaleLowerCase())) {
+			return `${workspaceFolderPath}/${name}`;
+		} else {
+			if (type === vscode.FileType.File) {return '';}
+		  const found = await findModelFolder(`${workspaceFolderPath}/${name}`);
+			if (found) {
+				return found;
+			}
+		}
+	}
+
+	return '';
+
+};
+
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
